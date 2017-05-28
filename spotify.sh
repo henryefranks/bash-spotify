8569758d316275630ba0c0e22deb5c1e2d32c99b
henryefranks <henryefranks@gmail.com>
2017-05-28 19:47:32 +0100

    Added live player support and the ability to toggle whether the album name is displayed. Also made a couple of other minor tweaks.

--- spotify.sh
+++ spotify.sh
@@ -4,8 +4,16 @@
 #    -Add messages when toggling play/pause/shuffle
 #    -Fix 'play' command
 #    -Add support for song name picking
+#    -Fix player -- if song name + artist is wider than window, it will glitch the output
+#        -use tput cols to get number of columns for checking
 
-version="0.1.3"
+# Checking if running on a mac (stopping if not)
+if [ $( uname ) != "Darwin" ]; then
+    echo "Sorry, this only works on macOS"
+    exit 1
+fi
+
+version="0.1.4"
 year="2017"
 
 # Opening Spotify if it isn't open
@@ -23,9 +31,22 @@ BOLD=$( tput bold )
 ULINE=$( tput smul )
 REGULAR=$( tput sgr0 )
 
+# Checking is the album flag is active without allowing other flags
+# Also, setting the return of the AppleScript appropriately
+album='return name of current track & " | " & artist of current track'
+if [ "$#" -eq 1 ]; then
+    validNum=1
+else
+    validNum=0
+    if [ $2 == "-a" ]; then # A somewhat temporary solution, will fix to allow for other options later
+      validNum=1
+      album='return name of current track & " | " & artist of current track  & " | " & album of current track'
+    fi
+fi
+
 # Checking for non-player cases first will save time
-if [ "$1" == "help" ] || [ "$#" -ne 1 ]; then  # Most important case
-    echo "usage: ./spotify.sh [options]"
+if [ "$1" == "help" ] || [ "$validNum" -ne 1 ]; then  # Most important case
+    echo "usage: ./spotify.sh [options] [-a]"
     echo "options: info   - more info"
     echo "         help   - help (this screen)"
     echo "         track  - info about the currently playing track"
@@ -34,6 +55,8 @@ if [ "$1" == "help" ] || [ "$#" -ne 1 ]; then  # Most important case
     echo "         play   - play (doesn't work, use toggle)"
     echo "         pause  - pause"
     echo "         toggle - toggle play/pause"
+    echo "         player - live player"
+    echo "use -a flag to show album (off by default)"
     exit
 elif [ "$1" == "info" ]; then
     echo -e "${GREEN}Spotify for Bash v$version${NONE}"
@@ -43,6 +66,34 @@ elif [ "$1" == "info" ]; then
     exit
 fi
 
+# Player mode
+if [ $1 = "player" ]; then
+    resize -s 6 70 2>&1 > /dev/null
+    tput civis
+    clear
+
+    oldLen=0
+    len=0
+
+    command="$0 track" # Calling itself because I'm lazy
+    if [ "$#" -eq 2 ]; then # Support for album flag
+	     command="$0 track -a"
+    fi
+    while :
+    do
+      oldLen=$len
+      tput cup -0
+      output=$(eval $command)
+      len=$(echo -n $output | wc -m)
+      if [ "$oldLen" != "$len" ]; then
+        # Clearing output if not properly overwritten to avoid graphical glitches
+	      clear
+	    fi
+	    echo "$output"
+	    sleep 0.1 # Using a 10Hz refresh rate to keep the second intervals regular
+    done
+fi
+
 echo -en ${BOLD}
 
 function showBar {
@@ -50,21 +101,24 @@ function showBar {
     echo -en "${NONE}"
     echo -n "$currentMin:"
     if (( $currentSec < 10 )); then
-	echo -n "0"
+      echo -n "0"
+    fi
+    if [ "$currentSec" == "-0" ]; then # Fixing a bug where it would display, for example 1:0-0 (currentSec was set to -0)
+      currentSec="0"
     fi
     echo -n "$currentSec"
     echo -en " [${GREEN}"
     for i in {0..20}; do
-	if (( i > lineLength )); then
-	    echo -en "${NONE}${BOLD}-${REGULAR}"
-	else
-	    echo -en "${GREEN}${BOLD}=${REGULAR}"
-	fi
+      if (( i > lineLength )); then
+        echo -en "${NONE}${BOLD}-${REGULAR}"
+      else
+	      echo -en "${GREEN}${BOLD}=${REGULAR}"
+	    fi
     done
     echo -en "${NONE}] "
     echo -n "$endMin:"
     if (( $endSec < 10 )); then
-	echo -n "0"
+	     echo -n "0"
     fi
     echo "$endSec"
 }
@@ -89,15 +143,24 @@ function nowPlaying {
     endSec=$( printf "%.*f" 0 $( echo "$truncDur - $(( endMin * 60 ))" | bc -l ) )
     echo "Now Playing:"
     echo -en ${GREEN}
-    osascript -e 'tell application "Spotify"' -e 'return name of current track & " | " & artist of current track  & " | " & album of current track' -e "end tell"
+
+    track=$(osascript -e 'tell application "Spotify"' -e "$album" -e "end tell")
+    if [ $(tput cols) -lt 45 ]; then
+      resize -s $(tput lines) 45 2>&1 > /dev/null
+    fi
+    if [ $(tput lines) -lt 6 ]; then
+      resize -s 6 $(tput cols) 2>&1 > /dev/null
+      clear
+    fi
+    echo ${track:0:$(tput cols)}
     state=$( osascript -e 'tell application "Spotify"' -e 'return player state' -e 'end tell' )
     echo
     # Note: I've reversed the play and pause logos so they are the way they appear in most players (pause logo = playing, play logo = paused)
     echo -n "("
     if [ "$state" == "playing" ]; then
-	echo -n "||"
+	     echo -n "||"
     else
-	echo -en "\xE2\x96\xB6"
+	     echo -en "\xE2\x96\xB6"
     fi
     echo -n ")  "
     showBar
